9f783772fcce663f0f8fe1842a25f9fe2d5836b1
smcviltco <75363152+smcviltco@users.noreply.github.com>
2021-05-02 14:33:24 +0500

    Add files via upload

--- smc_overall/models/account.py
+++ smc_overall/models/account.py
@@ -13,3 +13,29 @@ class AccountMoveInh(models.Model):
         for rec in self:
             invoices = self.env['res.partner'].search([('id', '=', rec.partner_id.id)])
             rec.customer_balance = invoices.total_due
+
+
+# class AccountPaymentInh(models.Model):
+#     _inherit = 'account.payment'
+#
+#     user_id = fields.Many2one('res.users')
+#     sale_ids = fields.Many2many('sale.order')
+#
+#     @api.onchange('sale_ids')
+#     def onchange_get_customer(self):
+#         if self.sale_ids:
+#             for rec in self:
+#                 rec.user_id = rec.sale_ids[0].user_id.id
+#                 rec.partner_id = rec.sale_ids[0].partner_id.id
+#
+#
+# class StockPickingInh(models.Model):
+#     _inherit = 'stock.picking'
+#
+#     def action_assign(self):
+#         payments = self.env['account.payment'].search([('partner_id', '=', self.partner_id.id), ('state', '=', 'posted')])
+#         if payments:
+#             sale_order = self.env['sale.order'].search([('name', '=', self.origin)])
+#             for rec in payments:
+#                 if sale_order.id in rec.sale_ids.ids:
+#                     record = super(StockPickingInh, self).action_assign()
