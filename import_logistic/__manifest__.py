2d9e16db2da9574bd97e9441cd727552eafc6f50
smcviltco <75363152+smcviltco@users.noreply.github.com>
2021-07-26 14:25:19 +0500

    Add files via upload

---
+++ import_logistic/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name': 'import_logistic Module',
+    'version': '0.2',
+    'category': 'accouting',
+    'license': "AGPL-3",
+    'summary': " ",
+    'author': 'Itech Reosurces',
+    'company': 'ItechResources',
+    'depends': [
+                'account',
+                'purchase',
+'stock_landed_costs',
+#                 'obro_workflows',
+                ],
+    'data': [
+            'security/ir.model.access.csv',
+            'data/lc_sequence.xml',
+            # 'views/account_invoice.xml',
+            'views/lc_view.xml',
+            'reports/landed_cost.xml',
+            ],
+    'installable': True,
+    'auto_install': False,
+}
