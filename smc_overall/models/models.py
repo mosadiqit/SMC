b78f3b492738dde556dd3802e2174566a6c6d82f
smcviltco <75363152+smcviltco@users.noreply.github.com>
2021-07-30 17:52:50 +0500

    Add files via upload

--- smc_overall/models/models.py
+++ smc_overall/models/models.py
@@ -19,8 +19,7 @@ class ResBranchInh(models.Model):
 class ResPartnerInh(models.Model):
     _inherit = 'res.partner'
 
-    customer_code = fields.Char('Customer Code', required=True, copy=False,
-                                index=True, default=lambda self: _('New'))
+    # customer_code = fields.Char('Customer Code', copy=False, index=True)
     test = fields.Boolean("Test Field")
     no_cnic = fields.Char('CNIC')
     ntn = fields.Char('NTN')
@@ -42,27 +41,27 @@ class ResPartnerInh(models.Model):
         else:
             self.is_supplier = False
 
-    @api.constrains('customer_code')
-    def check_code(self):
-        if self.customer_code:
-            code = self.env['res.partner'].search([('customer_code', '=', self.customer_code)])
-            if len(code) > 1:
-                raise UserError('User Already Exist')
+    # @api.constrains('customer_code')
+    # def check_code(self):
+    #     if self.customer_code:
+    #         code = self.env['res.partner'].search([('customer_code', '=', self.customer_code)])
+    #         if len(code) > 1:
+    #             raise UserError('User Already Exist')
 
-    def name_get(self):
-        res = []
-        for rec in self:
-            res.append((rec.id, '%s : %s : %s' % (rec.customer_code, rec.name, str(rec.total_due))))
-        return res
-
-    @api.model
-    def create(self, vals):
-        if vals.get('customer_code', _('New')) == _('New'):
-            vals['customer_code'] = self.env['ir.sequence'].next_by_code('res.partner.sequence') or _('New')
-        branch = self.env['res.branch'].browse([vals.get('branch_id')])
-        vals['customer_code'] = str(1) + '-' + str(self.env.user.agent_code) + '-' + str(branch.branch_code) + vals['customer_code']
-        result = super(ResPartnerInh, self).create(vals)
-        return result
+    # def name_get(self):
+    #     res = []
+    #     for rec in self:
+    #         res.append((rec.id, '%s : %s : %s' % (rec.customer_code, rec.name, str(rec.total_due))))
+    #     return res
+    #
+    # @api.model
+    # def create(self, vals):
+    #     if vals.get('customer_code', _('New')) == _('New'):
+    #         vals['customer_code'] = self.env['ir.sequence'].next_by_code('res.partner.sequence') or _('New')
+    #     branch = self.env['res.branch'].browse([vals.get('branch_id')])
+    #     vals['customer_code'] = str(1) + '-' + str(self.env.user.agent_code) + '-' + str(branch.branch_code) + vals['customer_code']
+    #     result = super(ResPartnerInh, self).create(vals)
+    #     return result
 
 
 class StockPickingInh(models.Model):
