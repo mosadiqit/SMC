72b4c58307a2686f5abdd62fa470d5525bcb0c49
smcviltco <75363152+smcviltco@users.noreply.github.com>
2021-09-23 01:08:19 +0500

    Add files via upload

--- confirm_transfer/models/models.py
+++ confirm_transfer/models/models.py
@@ -6,9 +6,12 @@ from odoo import models, fields, api
 class StockPickingInh(models.Model):
     _inherit = 'stock.picking'
 
+    is_created = fields.Boolean()
+
     def create_stock_picking(self):
         for rec in self:
-            print('Hello')
-            rec.sudo().action_confirm()
-            rec.sudo().button_validate()
+            if not rec.is_created:
+                rec.sudo().action_confirm()
+                rec.sudo().button_validate()
+                rec.is_created = True
 
