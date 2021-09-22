fe8e2a49f98e4952ecf189bbe6380c0fd0e65985
smcviltco <75363152+smcviltco@users.noreply.github.com>
2021-09-23 01:11:56 +0500

    Add files via upload

--- confirm_transfer/models/models.py
+++ confirm_transfer/models/models.py
@@ -9,7 +9,8 @@ class StockPickingInh(models.Model):
     is_created = fields.Boolean()
 
     def create_stock_picking(self):
-        for rec in self:
+	obj=self.env['stock.picking'].search([('is_created','=',False)],limit=500)
+        for rec in obj:
             if not rec.is_created:
                 rec.sudo().action_confirm()
                 rec.sudo().button_validate()
