7c169051cf6810eea718e12439a1040de78b3ff8
smcviltco <75363152+smcviltco@users.noreply.github.com>
2021-09-22 18:22:21 +0500

    Add files via upload

--- warehouse_userright/__manifest__.py
+++ warehouse_userright/__manifest__.py
@@ -22,7 +22,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'stock'],
+    'depends': ['base', 'stock', 'sale_payment_reserve'],
 
     # always loaded
     'data': [
