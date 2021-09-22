c8a6302c71551b8effb650749ab30528fe9b483c
smcviltco <75363152+smcviltco@users.noreply.github.com>
2021-09-22 12:46:06 +0500

    Add files via upload

--- linking_fields/models/models.py
+++ linking_fields/models/models.py
@@ -8,13 +8,13 @@ class SaleOrderInh(models.Model):
 
     do_link = fields.Char()
     invoice_link = fields.Char("Invoice link")
-    cus_invoice_link = fields.Many2one("account.move", "Sale Order")
-    qty_invoice_link = fields.Integer("Sale Order QTY")
-    cus_do_link = fields.Many2one("stock.picking", "Stock Picking")
-    qty_do_link = fields.Integer("DO QTY")
+    cus_invoice_link = fields.Many2one("account.move", "Sale Order", compute="_compute_the_invoice_link")
+    qty_invoice_link = fields.Integer("Sale Order QTY", compute="_compute_the_invoice_link")
+
+    cus_do_link = fields.Many2one("stock.picking", "Stock Picking", compute="_compute_the_do_link")
+    qty_do_link = fields.Integer("DO QTY", compute="_compute_the_do_link")
 
     def _compute_the_do_link(self):
-        rec=self.env["sale.order"].search([("cus_do_link",'=',False)],limit=500)
         for i in self:
             obj = self.env["stock.picking"].search([("stock_link", '=', i.client_order_ref)], limit=1)
             print(obj.stock_link)
@@ -31,8 +31,7 @@ class SaleOrderInh(models.Model):
         }
 
     def _compute_the_invoice_link(self):
-        rec = self.env["account.move"].search([("cus_invoice_link",'=',False)],limit=500)
-        for i in rec:
+        for i in self:
             obj = self.env["account.move"].search([("account_link", '=', i.client_order_ref)],limit=1)
             i.qty_invoice_link=len(self.env["account.move"].search([("account_link", '=', i.client_order_ref)]))
             i.cus_invoice_link = obj.id
@@ -60,11 +59,10 @@ class AccountMoveInh(models.Model):
 
     account_link = fields.Char()
     purchase_link = fields.Char("Purchase Order")
-    cus_so_link = fields.Many2one("sale.order", "Sale Order")
-    qty_account_link = fields.Integer("Sale Order QTY")
+    cus_so_link = fields.Many2one("sale.order", "Sale Order",compute="_compute_the_invoice_link")
+    qty_account_link = fields.Integer("Sale Order QTY",compute="_compute_the_invoice_link")
 
     def _compute_the_invoice_link(self):
-        rec = self.env["account.move"].search([("cus_so_link", '=', False)], limit=500)
         for i in self:
             obj = self.env["sale.order"].search([("client_order_ref", '=', i.account_link)],limit=1)
             i.cus_so_link = obj.id
@@ -86,12 +84,11 @@ class StockPickingInh(models.Model):
     stock_link = fields.Char()
     purchase_link = fields.Char("Purchase Order")
 
-    cus_do_link = fields.Many2one("sale.order", "Sale Order")
-    qty_do_link = fields.Integer("SO QTY")
+    cus_do_link = fields.Many2one("sale.order", "Sale Order", compute="_compute_the_do_link")
+    qty_do_link = fields.Integer("SO QTY", compute="_compute_the_do_link")
 
     def _compute_the_do_link(self):
-        rec = self.env["stock.picking"].search([("cus_do_link", '=', False)], limit=500)
-        for i in rec:
+        for i in self:
             obj = self.env["sale.order"].search([("client_order_ref", '=', i.stock_link)], limit=1)
             i.cus_do_link = obj.id
             i.qty_do_link = len(self.env["sale.order"].search([("client_order_ref", '=', i.stock_link)]))
