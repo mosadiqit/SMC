ec37e18cbecc64612b5b9a33778c2a93df2ef1b9
smcviltco <75363152+smcviltco@users.noreply.github.com>
2021-09-29 20:32:19 +0500

    Add files via upload

--- product_article_finish_no/model/product.py
+++ product_article_finish_no/model/product.py
@@ -104,8 +104,3 @@ class StockMoveLine(models.Model):
     _inherit = 'stock.move.line'
 
     sqm_box = fields.Float(string="SQM/Box", related='product_id.sqm_box')
-    total_sqm = fields.Float(string="Total Box", compute='_compute_product_uom_qty')
-
-    @api.depends('product_uom_qty')
-    def _compute_product_uom_qty(self):
-        self.total_sqm = self.product_uom_qty / (self.sqm_box or 1)
