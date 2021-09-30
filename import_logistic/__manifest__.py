2daa237203652e1984efc8c607f8f05e8e49020e
smcviltco <75363152+smcviltco@users.noreply.github.com>
2021-09-30 16:53:07 +0500

    Add files via upload

--- import_logistic/__manifest__.py
+++ import_logistic/__manifest__.py
@@ -14,6 +14,7 @@
     ],
     'data': [
         'security/ir.model.access.csv',
+        'security/security.xml',
         'data/lc_sequence.xml',
         # 'views/account_invoice.xml',
         'views/lc_view.xml',
