bfd8b8c72cdf257ba7bf65928e75aa53bd879429
smcviltco <75363152+smcviltco@users.noreply.github.com>
2021-10-08 17:19:07 +0500

    Add files via upload

--- sale_payment_reserve/__manifest__.py
+++ sale_payment_reserve/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'sale', 'account', 'branch', 'sales_consultant_user_rights'],
+    'depends': ['base', 'sale', 'account', 'branch'],
 
     # always loaded
     'data': [
