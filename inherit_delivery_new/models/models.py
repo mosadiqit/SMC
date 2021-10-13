31a69873312add7570fa77cf3349e4af46dc160d
smcviltco <75363152+smcviltco@users.noreply.github.com>
2021-10-13 18:31:00 +0500

    Add files via upload

--- inherit_delivery_new/models/models.py
+++ inherit_delivery_new/models/models.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from datetime import datetime
+from pytz import timezone
 
 
 class InheritDelivery(models.Model):
@@ -14,3 +16,8 @@ class InheritPicking(models.Model):
     _inherit = 'stock.picking'
 
     current_user = fields.Many2one('res.users', 'Current User', default=lambda self: self.env.user)
+
+    def get_print_date(self):
+        now_utc_date = datetime.now()
+        now_dubai = now_utc_date.astimezone(timezone('Asia/Karachi'))
+        return now_dubai.strftime('%d/%m/%Y %H:%M:%S')
