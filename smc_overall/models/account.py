cfed62ddbf23fc9318da403b29b7bbb28c293243
smcviltco <75363152+smcviltco@users.noreply.github.com>
2021-10-13 16:52:16 +0500

    Add files via upload

--- smc_overall/models/account.py
+++ smc_overall/models/account.py
@@ -13,3 +13,22 @@ class AccountMoveInh(models.Model):
         for rec in self:
             invoices = self.env['res.partner'].search([('id', '=', rec.partner_id.id)])
             rec.customer_balance = invoices.total_due
+
+
+class StockLandedInh(models.Model):
+    _inherit = 'stock.landed.cost'
+
+    vendor_bill_ids = fields.Many2many('account.move')
+
+    def action_show_line(self):
+        vals_list = []
+        for rec in self.vendor_bill_ids:
+            for line in rec.invoice_line_ids:
+                vals_list.append([0, 0, {
+                    'account_id': line.account_id.id,
+                    'product_id': line.product_id.id,
+                    'name': line.name,
+                    'price_unit': line.price_unit,
+                    'split_method': 'equal',
+                }])
+        self.cost_lines = vals_list
