3ac8dba968ba1337d685a861bff03ea5c62bd18a
smcviltco <75363152+smcviltco@users.noreply.github.com>
2021-10-20 18:20:34 +0500

    Add files via upload

--- sale_order_reports/models/models.py
+++ sale_order_reports/models/models.py
@@ -7,3 +7,14 @@ class ResCompanyInh(models.Model):
     _inherit = 'res.company'
 
     cs = fields.Char('CS')
+
+
+class SaleOrderInh(models.Model):
+    _inherit = 'sale.order'
+
+    def get_mobile(self, user):
+        employee = self.env['hr.employee'].search([('user_id', '=', user.id)], limit=1)
+        if employee:
+            return employee.mobile_phone
+        else:
+            return ''
