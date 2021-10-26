fb86f916ed0919ce0c7cef3132cd07f81fd0459e
smcviltco <75363152+smcviltco@users.noreply.github.com>
2021-10-26 18:29:05 +0500

    Add files via upload

--- de_partner_ledger/models/partner_ledger.py
+++ de_partner_ledger/models/partner_ledger.py
@@ -23,7 +23,7 @@ class PartnerLedger(models.TransientModel):
     partner_id = fields.Many2one('res.partner', string='Partner', required=True, help='Select Partner for movement')
 
     def print_report(self):
-        data = {'partner_id': self.partner_id.id,'start_date': self.start_date, 'end_date': self.end_date}
+        data = {'partner_id': self.partner_id.id, 'start_date': self.start_date, 'end_date': self.end_date}
         return self.env.ref('de_partner_ledger.partner_ledger_pdf').report_action(self, data)
 
 
--- de_partner_ledger/report/partner_ledger_report.py
+++ de_partner_ledger/report/partner_ledger_report.py
@@ -1,57 +1,51 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Odoo, Open Source Management Solution
-#    Copyright (C) 2015 Dynexcel (<http://dynexcel.com/>).
-#
-##############################################################################
-import pytz
-import time
 
-from odoo import models, fields, api
-from odoo.exceptions import ValidationError, Warning
-from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT, DEFAULT_SERVER_DATE_FORMAT
-from datetime import datetime
+
+from odoo import models
 
 
 class CustomReport(models.AbstractModel):
     _name = "report.de_partner_ledger.de_partner_ledger_pdf_report"
 
-    def _get_report_values(self,docids,data=None):
-        query_get_location = ''
-        
-        cr = self._cr
-        query = """select sum(l.debit - l.credit) as opening_bal
-from account_move_line l
-join account_move m on l.move_id = m.id
-join account_account a on l.account_id = a.id
-where a.reconcile = True
-        and l.partner_id = %s and l.date < %s
-        """ 
-        cr.execute(query, [data['partner_id'],data['start_date']])
-        openbal = cr.dictfetchall()
+    def get_partner_bal(self, data):
+        partner_ledger = self.env['account.move.line'].search(
+            [('partner_id', '=', data['partner_id']),('date', '>=', data['start_date']),('date', '<=', data['end_date']),
+             ('move_id.state', '=', 'posted'), ('full_reconcile_id', '=', False), ('balance', '!=', 0),
+             ('account_id.reconcile', '=', True), ('full_reconcile_id', '=', False), '|',
+             ('account_id.internal_type', '=', 'payable'), ('account_id.internal_type', '=', 'receivable')])
+        return partner_ledger
 
-        cr = self._cr
-        query = """
-        select m.ref,m.name as doc_no, m.date, m.narration, j.name as journal, p.name as partner_name, 
-l.name as line_desc, a.name as gl_account, m.currency_id, l.debit, l.credit
-from account_move_line l
-join account_move m on l.move_id = m.id
-join res_partner p on l.partner_id = p.id
-join account_account a on l.account_id = a.id
-join account_journal j on m.journal_id = j.id
-where a.reconcile = True
-        and l.partner_id = %s and (m.date between %s and %s)
-        order by m.date
-        """ 
-        cr.execute(query, [data['partner_id'],data['start_date'],data['end_date']])
-        dat = cr.dictfetchall()
-        print(data)
+    def get_opening_bal(self, data):
+        open_bal = self.env['account.move.line'].search(
+            [('partner_id', '=', data['partner_id']),('date', '<=', data['start_date']),
+             ('move_id.state', '=', 'posted'), ('full_reconcile_id', '=', False), ('balance', '!=', 0),
+             ('account_id.reconcile', '=', True), ('full_reconcile_id', '=', False), '|',
+             ('account_id.internal_type', '=', 'payable'), ('account_id.internal_type', '=', 'receivable')])
+        bal = 0
+        for rec in open_bal:
+            bal = bal + rec.balance
+        return bal
 
+    def get_closing_bal(self, data):
+        open_bal = self.env['account.move.line'].search(
+            [('partner_id', '=', data['partner_id']), ('date', '>=', data['start_date']), ('date', '<=', data['end_date']),
+             ('move_id.state', '=', 'posted'), ('full_reconcile_id', '=', False), ('balance', '!=', 0),
+             ('account_id.reconcile', '=', True), ('full_reconcile_id', '=', False), '|',
+             ('account_id.internal_type', '=', 'payable'), ('account_id.internal_type', '=', 'receivable')])
+        bal = 0
+        for rec in open_bal:
+            bal = bal + rec.balance
+        return bal
+
+    def _get_report_values(self, docids, data=None):
+        dat = self.get_partner_bal(data)
+        openbal = self.get_opening_bal(data)
+        closingbal = self.get_closing_bal(data)
         return {
             'doc_ids': self.ids,
             'doc_model': 'partner.ledger',
             'openbal': openbal,
+            'closingbal': closingbal + openbal,
             'dat': dat,
             'data': data,
         }
