94011622bcaa2eeb61380af64f955070242caa3b
smcviltco <75363152+smcviltco@users.noreply.github.com>
2021-11-16 17:15:21 +0500

    Add files via upload

--- invoice_smc/models/models.py
+++ invoice_smc/models/models.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from datetime import datetime
+from pytz import timezone
 
 
 class invone_smc(models.Model):
@@ -15,5 +17,10 @@ class InheritField(models.Model):
 
     freight = fields.Char(string='Details')
     journal_id = fields.Many2one("account.journal",string='Journal id')
+
+    def get_print_date(self):
+        now_utc_date = datetime.now()
+        now_dubai = now_utc_date.astimezone(timezone('Asia/Karachi'))
+        return now_dubai.strftime('%d/%m/%Y %H:%M:%S')
 
 
