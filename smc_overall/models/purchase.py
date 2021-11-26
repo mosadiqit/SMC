c23abe316eec1222eb07a5d87e79e94ed6a1fd32
smcviltco <75363152+smcviltco@users.noreply.github.com>
2021-11-26 17:17:35 +0500

    Add files via upload

--- smc_overall/models/purchase.py
+++ smc_overall/models/purchase.py
@@ -10,4 +10,7 @@ class PurchaseOrderInh(models.Model):
         ('on_the_way', 'On the Way to Khi'),
         ('out_of_way', 'Out of way to Lhr'),
         ('arrived', 'Arrived'),
+        ('custom', 'Custom'),
     ], string='Status Ref')
+
+    manual_status = fields.Char('Manual Status')
