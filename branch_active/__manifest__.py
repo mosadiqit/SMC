7807590d562f39ef4f802b3f4aa3cd36be143410
smcviltco <75363152+smcviltco@users.noreply.github.com>
2021-12-27 11:46:22 +0500

    Add files via upload

--- branch_active/__manifest__.py
+++ branch_active/__manifest__.py
@@ -20,16 +20,12 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'branch'],
+    'depends': ['base', 'branch', 'branch_report'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
     ],
 }
--- branch_active/models/models.py
+++ branch_active/models/models.py
@@ -8,3 +8,9 @@ class ResBranchInh(models.Model):
 
     # branch_code = fields.Char('Branch Code')
     active = fields.Boolean(default=True)
+
+
+class BranchReportInh(models.TransientModel):
+    _inherit = 'branch.wizard'
+
+    branches = fields.Many2many('res.branch', default=lambda self: self.env.user.branch_id.ids)
